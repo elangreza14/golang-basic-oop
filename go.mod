e6fb357b2bd296dd31aa94f3df5c4044a9da609c
elangreza14 <rezaelangerlangga14@gmail.com>
2022-05-07 21:23:39 +0700

    first commit

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/elangreza14/golang-basic-oop
 
 go 1.18
+
+require (
+	github.com/banzaicloud/logrus-runtime-formatter v0.0.0-20190729070250-5ae5475bae5e // indirect
+	github.com/elangreza14/golang-logrus-custom v0.0.0-20220507140907-5ab70363331a // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
+)
--- main.go
+++ main.go
@@ -6,24 +6,40 @@ import (
 	"github.com/elangreza14/golang-basic-oop/bank"
 	"github.com/elangreza14/golang-basic-oop/conventional"
 	"github.com/elangreza14/golang-basic-oop/internet"
+	elangrezaLogger "github.com/elangreza14/golang-logrus-custom"
 )
 
 func main() {
+	logger := elangrezaLogger.NewLogger([]elangrezaLogger.PrefixLoggerName{
+		{
+			Title:       "app",
+			Description: "golang basic oop",
+		}})
 
 	allBank := []bank.IBank{
 		bank.NewBank(conventional.NewConventional("reza conven", 0.1)),
 		bank.NewBank(internet.NewInternet("reza internet")),
 	}
+	fmt.Println(allBank)
 
 	for _, v := range allBank {
 		err := v.FlowMoney(1000)
-		fmt.Println(err)
+		if err != nil {
+			logger.Error("ERR: %v", err)
+		}
+
 		a := v.GetBallance()
-		fmt.Println(a)
+
+		logger.Info("INFO BALLANCE: %v", a)
+
 		err = v.FlowMoney(-900)
-		fmt.Println(err)
+		if err != nil {
+			logger.Error("ERR: %v", err)
+		}
+
 		a = v.GetBallance()
-		fmt.Println(a)
+		logger.Info("INFO BALLANCE: %v", a)
+
 	}
 
 }
